5b07f4d916a3eab5f0d7dff51e041332cae0b030
Dariazrn <78564075+Dariazrn@users.noreply.github.com>
2022-12-04 23:42:47 -0500

    Add files via upload

--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -20,21 +20,18 @@ export default function RouteGuard(props) {
   }
 
   useEffect(() => {
-    // on initial load - run auth check
+
     authCheck(router.pathname);
     updateAtoms();
 
-    // on route change complete - run auth check
-    router.events.on("routeChangeComplete", authCheck);
 
-    // unsubscribe from events in useEffect return function
+    router.events.on("routeChangeComplete", authCheck);
     return () => {
       router.events.off("routeChangeComplete", authCheck);
     };
   }, []);
 
   function authCheck(url) {
-    // redirect to login page if accessing a private page and not logged in
     const path = url.split("?")[0];
     if (!isAuthenticated() && !PUBLIC_PATHS.includes(path)) {
       setAuthorized(false);
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
+*  WEB422 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
 * 
-*  Name: Daria Zyrianova Student ID: 121782205 Date: November 18, 2022
-*
+*  Name: Daria Zyrianova Student ID: 121782205 Date: December 4, 2022
+*  Vercel App (Deployed) Link: https://a6-fxd8rgc9a-dariazrn.vercel.app/
 *
 ********************************************************************************/ 
 
